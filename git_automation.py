ceb2ad6525f1ca530d69dd0168c9fc378b3f9291
Do Thien Hoang <theksbd@gmail.com>
2024-01-29 00:28:52 +0700

    replace git_automation script

--- git_automation.py
+++ git_automation.py
@@ -1,20 +1,23 @@
 # import os
 import subprocess
 
-# Get the current directory
+# Get the current directory 
 # curdir = os.getcwd()
 
 # Navigate back to the Git root folder
-# os.chdir(os.path.join(curdir, "../../"))
+# os.chdir(os.path.join(curdir, "../"))
 
 # Run git status
 subprocess.run(["git", "status"])
 
-# Run git add .
+# Run git add . 
 subprocess.run(["git", "add", "."])
 
-# Run git commit -m "update"
-subprocess.run(["git", "commit", "-m", "update"])
+# Get user input for the commit message
+commit_message = input("Enter your commit message: ")
+
+# Run git commit with the custom message
+subprocess.run(["git", "commit", "-m", commit_message])
 
 # Run git push
 subprocess.run(["git", "push"])
